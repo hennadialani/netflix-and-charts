ff1066a0261c72985b281f9c1cd10a681a7ddf6e
hennadialani <hennadialani@ucla.edu>
2019-01-19 15:34:26 -0800

    some comments as to what we plan to do!
    we need some input data tho to play around with and muck up!

--- ui.R
+++ ui.R
@@ -16,6 +16,15 @@ shinyUI(fluidPage(
   
   # Application title
   titlePanel("Old Faithful Geyser Data"),
+  # Tidying up your data with Marie Kondo. Let's spark some joy with a Netflix 2018 in review.
+  # 2018 in review because Netflix doesn't give it to you! 
+  
+  # Dotplot of 2018 - here's the unique content you viewed this year.
+  # Donut chart for genres. 
+  # _____ % of what you watched in 2018 are movies. (Notes: We counted comedy specials as movies.) ____ are TV shows. 
+  # Assuming ____ you have watched ___ 
+  
+  # On this day you watched ____ (max titles). Either you were indecisive or you were glued to your TV. Are you still watching?
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
